763e01b262a8cefc82469599c1cb2e6ff3877994
Stig Johan Berggren <github@stigjohan.no>
2026-03-08 21:44:06 +0100

    skill ut filtrerte filialer i tilghengelighet

--- components/Copies.tsx
+++ components/Copies.tsx
@@ -2,24 +2,42 @@ import { useContext } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { BranchContext } from "@/lib/branchContext";
 import type { Copy } from "@/lib/Copy";
+import { FilterContext } from "@/lib/filterContext";
 
 export function Copies({ copies }: { copies: Copy[] }) {
   const { getBranchName } = useContext(BranchContext);
-  const parts = copies.map(({ locLabel, shelfmark, branchcode }) => {
+  const { filters } = useContext(FilterContext);
+  const activeBranches = [];
+  const inactiveBranches = [];
+  for (const copy of copies) {
+    const { locLabel, shelfmark, branchcode } = copy;
     const branch = getBranchName(branchcode);
     const key = [locLabel, shelfmark, branchcode].join("_");
-    return (
+    const part = (
       <View style={styles.group} key={key}>
         <Text style={styles.title}>{branch}</Text>
         <Text>{shelfmark}</Text>
         <Text>{locLabel}</Text>
       </View>
     );
-  });
-  return <View>{parts}</View>;
+    if (filters[branchcode]) {
+      activeBranches.push(part);
+    } else {
+      inactiveBranches.push(part);
+    }
+  }
+  return (
+    <View>
+      <View>{activeBranches}</View>
+      {inactiveBranches.length > 0 && (
+        <View style={styles.inactive}>{inactiveBranches}</View>
+      )}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   group: { margin: 10 },
   title: { fontWeight: 700 },
+  inactive: { borderTopColor: "#666", borderTopWidth: 1, opacity: 0.4 },
 });
